ee29841ec2439022e59cdc575fb66e38a634ad0a
SimSchwebel <simon.schwebel@ecam-strasbourg.eu>
2021-11-08 15:01:55 +0100

    test

--- Percolation/Percolation.cs
+++ Percolation/Percolation.cs
@@ -27,26 +27,85 @@ namespace Percolation
 
         public bool IsOpen(int i, int j)
         {
+            if (_open[i, j])
+            {
+                return true;
+            }
+
             return false;
         }
 
         public bool IsFull(int i, int j)
         {
+            if (_full[i, j])
+            {
+                return true;
+            }
+
             return false;
         }
 
         public bool Percolate()
         {
+            for (int i = 0; i < _size; i++)
+            {
+                if (_full[i, _size])
+                {
+                    Console.WriteLine("Il y a percolation.");
+                    return true;
+                }
+
+                Console.WriteLine("Il n'y a pas de percolation.");
+                return false;
+
+            }
+
             return false;
         }
 
         private List<KeyValuePair<int, int>> CloseNeighbors(int i, int j)
         {
-            return null;
+            List<KeyValuePair<int, int>> _voisins = new List<KeyValuePair<int, int>>();
+
+
+            if (i > 0)
+            {
+                _voisins.Add(new KeyValuePair<int, int>(i - 1, j));
+            }
+
+            if (i < _size - 1)
+            {
+                _voisins.Add(new KeyValuePair<int, int>(i + 1, j));
+            }
+
+            if (j > 0)
+            {
+                _voisins.Add(new KeyValuePair<int, int>(i, j - 1));
+            }
+
+            if (j < _size - 1)
+            {
+                _voisins.Add(new KeyValuePair<int, int>(i, j + 1));
+            }
+
+            return _voisins;
+
+
         }
 
         public void Open(int i, int j)
         {
+
+            if (IsOpen(i,j) != true)
+            {
+
+
+
+            }
+
+           
+
+                return false;
 
         }
     }
