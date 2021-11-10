660968a6b738496db434a06f381aa2e8a2c7684a
SimSchwebel <simon.schwebel@ecam-strasbourg.eu>
2021-11-10 11:01:14 +0100

    Test

--- Projet_1/Program.cs
+++ Projet_1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,18 @@ namespace Projet_1
     {
         static void Main(string[] args)
         {
+            string path = Directory.GetCurrentDirectory();
+            string acctPath = path + @"\Comptes_1.txt";
+            string trxnPath = path + @"\Transactions_1.txt";
+            string sttsPath = path + @"\Statut_1.txt";
+
+
+
+
+            // Keep the console window open
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
