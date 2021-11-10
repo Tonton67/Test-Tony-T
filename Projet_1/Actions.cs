38f54b3554d1f1af2e76bbe513ebd335d2f1e8d6
SimSchwebel <simon.schwebel@ecam-strasbourg.eu>
2021-11-10 14:59:41 +0100

    Test

---
+++ Projet_1/Actions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_1
+{
+    public static class Actions
+    {
+        public static List<Transaction> LectureTransaction(string trxnPath)
+        {
+            //Déclaration de la liste
+            List<Transaction> transactions = new List<Transaction>();
+
+            //Lecture du fichier Compte
+            string[] lines = File.ReadAllLines(trxnPath);
+
+            //Séparation des données
+            foreach (string line in lines)
+            {
+                Console.WriteLine($"Fichier : {line}");
+                string[] split = line.Split(';');
+                for (int i = 0; i < split.Length; i++)
+                {
+                    Console.WriteLine($" Infos Split : {split[i]}");
+                   
+
+                }
+                
+                //string transaction = split[1];
+
+                //Classement des données dans le dictionnaire
+                //if (!transactions.ContainsKey(transaction))
+                //{
+                //    transactions.Add(transaction, new List<int>());
+                //}
+            }
+            return transactions;
+        }
+
+    }
+}
---
+++ Projet_1/Statut.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_1
+{
+    public class Statut
+    {
+        public int Numero { get; set; }
+        public string Statuts { get; set; }
+        public Statut(int numero, string statut)
+        {
+            Numero = numero;
+            Statuts = statut;
+        }
+    }
+}
